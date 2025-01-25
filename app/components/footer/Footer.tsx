986f27dff2f5398b79dd074900468fd54185f8a6
Barış Muharrem Ateş <barisates_2002@hotmail.com>
2025-01-25 11:06:02 +0300

    Footer eklendi.(Düzenlenecek)

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,5 +1,22 @@
 ﻿const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-gray-800 text-white py-4 h-[60px]">
+      <div className="container mx-auto flex justify-between items-center">
+        <p>&copy; 2025 Tüm Hakları Saklıdır</p>
+        <div className="flex gap-4">
+          <a href="#" className="hover:underline">
+            Hakkımızda
+          </a>
+          <a href="#" className="hover:underline">
+            İletişim
+          </a>
+          <a href="#" className="hover:underline">
+            Gizlilik
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
