9c03431066b83b81638317d00d648e0b004fe6d7
Barış Muharrem Ateş <barisates_2002@hotmail.com>
2025-01-26 12:12:07 +0300

    İcon yönlendirmeleri eklendi.

--- app/components/navbar/Icons.tsx
+++ app/components/navbar/Icons.tsx
@@ -2,14 +2,23 @@
 import { FaInstagram } from "react-icons/fa6";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaYoutube } from "react-icons/fa";
+import Link from "next/link";
 
 const Icons = () => {
   return (
     <div className="hidden md:flex gap-3 relative cursor-pointer text-red-400 ">
-      <FaFacebook size={25} />
-      <FaInstagram size={25} />
-      <FaXTwitter size={25} />
-      <FaYoutube size={25} />
+      <Link href={"https://www.youtube.com/channel/UCPwIL9T5o63o7-X4pI5x_4A"}>
+        <FaYoutube size={25} />
+      </Link>
+      <Link href={"https://www.instagram.com/istcode/"}>
+        <FaInstagram size={25} />
+      </Link>
+      <Link href={"https://x.com/istcodecom"}>
+        <FaXTwitter size={25} />
+      </Link>
+      <Link href={"https://www.facebook.com/istcode/"}>
+        <FaFacebook size={25} />
+      </Link>
     </div>
   );
 };
