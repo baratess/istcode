1c496879af64c4b6afd0b332246607f1a581164e
Barış Muharrem Ateş <barisates_2002@hotmail.com>
2025-01-27 12:16:59 +0300

    Responsive düzenlemeleri

--- app/components/navbar/Navbar1.tsx
+++ app/components/navbar/Navbar1.tsx
@@ -1,20 +1,20 @@
 ﻿"use client";
 
 import Blogs from "./Blogs";
-import Home from "./AboutUs";
 import Menü from "./Menü";
 import Search from "./Search";
 import User from "./User";
 import Link from "next/link";
+import AboutUs from "./AboutUs";
 
 const Navbar: React.FC = () => {
   return (
     <div className="bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 pt-10 animate-fadeIn">
       <div className="relative">
         <header className="flex items-center justify-between px-6 py-3 bg-gradient-to-r from-gray-700 to-gray-800 text-white shadow-lg h-[120px] rounded-md max-w-screen-xl w-full mx-auto">
-          <div className="flex items-center gap-6">
+          <div className="flex items-center gap-6 w-full sm:w-auto">
             <Menü />
-            <div className="text-2xl font-extrabold">
+            <div className="text-2xl font-extrabold flex-grow sm:flex-grow-0 text-center sm:text-left">
               <Link href="/">
                 <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 hover:scale-105 transform transition duration-200 text-4xl">
                   İstcode
@@ -25,10 +25,10 @@ const Navbar: React.FC = () => {
 
           <div className="hidden md:flex flex-grow items-center justify-center gap-6">
             <Blogs />
-            <Home />
+            <AboutUs />
           </div>
 
-          <div className="flex items-center gap-3 ml-auto">
+          <div className="flex items-center gap-3 ml-auto sm:ml-0">
             <Search />
             <User />
           </div>
