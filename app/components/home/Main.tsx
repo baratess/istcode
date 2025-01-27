3e1b1b4121f4d12c301c775204560fe82bfd3e08
Barış Muharrem Ateş <barisates_2002@hotmail.com>
2025-01-27 11:23:19 +0300

    Mobile uyumluluk ayarlama.

--- app/components/home/Main.tsx
+++ app/components/home/Main.tsx
@@ -14,11 +14,12 @@ const HomePage: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 pt-20 animate-fadeIn">
-      <div className="flex w-full px-5 gap-8 mb-16">
-        <div className="flex flex-col items-center w-1/2 ml-10">
+      <div className="flex flex-wrap w-full px-5 gap-8 mb-16 md:flex-nowrap">
+        {/* Left Column */}
+        <div className="flex flex-col items-center w-full md:w-1/2 md:ml-10">
           <div
             onClick={() => handleCategoryClick("felsefe")}
-            className="animate-fadeIn"
+            className="animate-fadeIn w-full"
           >
             <Image
               src="/felsefe.jpg"
@@ -28,33 +29,34 @@ const HomePage: React.FC = () => {
               height={300}
             />
           </div>
-          <div className="text-white text-xl font-semibold text-center p-5 bg-gray-500 bg-opacity-50 rounded-lg shadow-lg mt-10">
+          <div className="text-white text-lg md:text-xl font-semibold text-center p-5 bg-gray-500 bg-opacity-50 rounded-lg shadow-lg mt-10">
             <span className="text-yellow-300">Felsefe: </span>Düşüncelerin
             derinliklerine inen blog yazılarını keşfedin.
           </div>
 
           <div
             onClick={() => handleCategoryClick("sanat")}
-            className="animate-fadeIn"
+            className="animate-fadeIn w-full"
           >
             <Image
               src="/sanat.jpeg"
               alt="Sanat"
-              className="rounded-lg shadow-2xl w-full object-cover transform transition duration-300 hover:scale-105 mt-20"
+              className="rounded-lg shadow-2xl w-full object-cover transform transition duration-300 hover:scale-105 mt-10 md:mt-20"
               width={400}
               height={300}
             />
           </div>
-          <div className="text-white text-xl font-semibold text-center p-5 bg-gray-500 bg-opacity-50 rounded-lg shadow-lg mt-10">
+          <div className="text-white text-lg md:text-xl font-semibold text-center p-5 bg-gray-500 bg-opacity-50 rounded-lg shadow-lg mt-10">
             <span className="text-red-200">Sanat:</span> Yaratıcılığın
             sınırlarını zorlayan blog yazılarını keşfedin.
           </div>
         </div>
 
-        <div className="flex flex-col items-center w-1/2 mr-10">
+        {/* Right Column */}
+        <div className="flex flex-col items-center w-full md:w-1/2 md:mr-10">
           <div
             onClick={() => handleCategoryClick("bilim")}
-            className="animate-fadeIn"
+            className="animate-fadeIn w-full"
           >
             <Image
               src="/Bilim.jpg"
@@ -64,24 +66,24 @@ const HomePage: React.FC = () => {
               height={300}
             />
           </div>
-          <div className="text-white text-xl font-semibold text-center p-5 bg-gray-500 bg-opacity-50 rounded-lg shadow-lg mt-10">
+          <div className="text-white text-lg md:text-xl font-semibold text-center p-5 bg-gray-500 bg-opacity-50 rounded-lg shadow-lg mt-10">
             <span className="text-green-400">Bilim:</span> Evreni anlamak için
             yazılmış blog yazılarını keşfedin.
           </div>
 
           <div
             onClick={() => handleCategoryClick("teknoloji")}
-            className="animate-fadeIn"
+            className="animate-fadeIn w-full"
           >
             <Image
               src="/teknoloji.jpg"
               alt="Teknoloji"
-              className="rounded-lg shadow-2xl w-full object-cover transform transition duration-300 hover:scale-105 mt-28"
+              className="rounded-lg shadow-2xl w-full object-cover transform transition duration-300 hover:scale-105 mt-10 md:mt-28"
               width={400}
               height={300}
             />
           </div>
-          <div className="text-white text-xl font-semibold text-center p-5 bg-gray-500 bg-opacity-50 rounded-lg shadow-lg mt-10">
+          <div className="text-white text-lg md:text-xl font-semibold text-center p-5 bg-gray-500 bg-opacity-50 rounded-lg shadow-lg mt-10">
             <span className="text-blue-200">Teknoloji:</span> Geleceği
             şekillendiren inovasyonlar hakkında yazılmış blog yazılarını
             keşfedin.
--- app/components/navbar/Navbar1.tsx
+++ app/components/navbar/Navbar1.tsx
@@ -13,6 +13,7 @@ const Navbar: React.FC = () => {
       <div className="relative">
         <header className="flex items-center justify-between px-6 py-3 bg-gradient-to-r from-gray-700 to-gray-800 text-white shadow-lg h-[120px] rounded-md max-w-screen-xl w-full mx-auto">
           <div className="flex items-center gap-6">
+            <Menü />
             <div className="text-2xl font-extrabold">
               <Link href="/">
                 <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 hover:scale-105 transform transition duration-200 text-4xl">
@@ -20,8 +21,6 @@ const Navbar: React.FC = () => {
                 </h1>
               </Link>
             </div>
-
-            <Menü />
           </div>
 
           <div className="hidden md:flex flex-grow items-center justify-center gap-6">
