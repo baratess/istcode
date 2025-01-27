c1baa5944a22c5d6f74bc7556d5e753dafe301b1
Barış Muharrem Ateş <barisates_2002@hotmail.com>
2025-01-27 12:22:30 +0300

    Detay sayfası responsive hale getirildi.

--- app/blogs/[blogCategory]/[blogId]/page.tsx
+++ app/blogs/[blogCategory]/[blogId]/page.tsx
@@ -47,8 +47,8 @@ const BlogDetails: React.FC = () => {
 
   if (post) {
     return (
-      <div className="px-4 md:px-10 py-8 flex justify-center items-center h-screen bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 animate-fadeIn">
-        <div className="p-6 rounded-lg shadow-2xl bg-slate-200 w-[700px] h-[720px] border-blue-200 border-4">
+      <div className="px-4 md:px-10 py-8 flex justify-center items-center min-h-screen bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 animate-fadeIn">
+        <div className="p-6 rounded-lg shadow-2xl bg-slate-200 w-full max-w-[700px] h-auto border-blue-200 border-4">
           <h1 className="text-4xl font-bold text-gray-800 mb-4 mt-10 pt-5 text-center">
             {post.title}
           </h1>
@@ -61,8 +61,8 @@ const BlogDetails: React.FC = () => {
             <p>{post.description}</p>
           </div>
 
-          <div className="mt-20 text-center">
-            <p className="text-2xl text-gray-600 p-10 pt-20">
+          <div className="mt-10 text-center px-5 sm:px-10">
+            <p className="text-xl text-gray-600 py-10">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
               quod nobis vitae voluptatum sed, facere accusantium odit, magnam,
               perferendis quos eius neque mollitia odio iste iusto quidem.
