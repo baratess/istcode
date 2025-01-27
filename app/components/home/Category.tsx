f97d4cc6eb8506cdc23df52c2153747e264f341f
Barış Muharrem Ateş <barisates_2002@hotmail.com>
2025-01-27 13:05:02 +0300

    Kategori kısmı responsive yapıldı

--- app/components/home/Category.tsx
+++ app/components/home/Category.tsx
@@ -17,7 +17,7 @@ const Category = () => {
   };
 
   return (
-    <div className="flex items-center justify-center px-3 md:px-10 gap-3 md:gap-10 py-5 md:py-8 overflow-x-auto">
+    <div className="flex items-center justify-center px-3 md:px-10 gap-3 md:gap-10 py-5 md:py-8 overflow-x-auto flex-wrap md:flex-nowrap">
       {uniqueCategories.map((category, index) => (
         <div
           className={`border font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500 rounded-full min-w-[100px] w-[200px] flex items-center justify-center cursor-pointer px-2 py-1 text-center hover:scale-105 transform transition duration-200 ${
